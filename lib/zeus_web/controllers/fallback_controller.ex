0fa71fc41faeebb17c6cb796ee94e5df284c6c74
Bustikiller <mayn13@gmail.com>
2019-01-05 12:44:19 +0100

    Render validation errors via API

--- lib/zeus_web/controllers/fallback_controller.ex
+++ lib/zeus_web/controllers/fallback_controller.ex
@@ -12,4 +12,26 @@ defmodule ZeusWeb.FallbackController do
     |> put_view(ZeusWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    errors = Enum.map(changeset.errors, fn {field, detail} ->
+      %{
+        field: %{ pointer: "/data/attributes/#{field}" },
+        title: "Invalid Attribute",
+        detail: render_detail(detail)
+      }
+    end)
+
+    json conn, %{errors: errors}
+  end
+
+  def render_detail({message, values}) do
+    Enum.reduce values, message, fn {k, v}, acc ->
+      String.replace(acc, "%{#{k}}", to_string(v))
+    end
+  end
+
+  def render_detail(message) do
+    message
+  end
 end
